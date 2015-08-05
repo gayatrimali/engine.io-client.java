a41f1f9703ad9121af07de527680563f6c09026f
gayatri.mali <gayatri.mali@olacabs.com>
2015-08-05 09:56:07 +0530

    Catched generic exception

--- src/main/java/com/github/nkzawa/engineio/client/transports/WebSocket.java
+++ src/main/java/com/github/nkzawa/engineio/client/transports/WebSocket.java
@@ -28,191 +28,194 @@ import static com.squareup.okhttp.ws.WebSocket.PayloadType.TEXT;
 
 public class WebSocket extends Transport {
 
-    public static final String NAME = "websocket";
-    private com.squareup.okhttp.ws.WebSocket ws;
-    private WebSocketCall wsCall;
+	public static final String NAME = "websocket";
+	private com.squareup.okhttp.ws.WebSocket ws;
+	private WebSocketCall wsCall;
 
-    public WebSocket(Options opts) {
-        super(opts);
-        this.name = NAME;
-    }
+	public WebSocket(Options opts) {
+		super(opts);
+		this.name = NAME;
+	}
 
-    protected void doOpen() {
-        Map<String, String> headers = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
-        this.emit(EVENT_REQUEST_HEADERS, headers);
+	protected void doOpen() {
+		Map<String, String> headers = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+		this.emit(EVENT_REQUEST_HEADERS, headers);
+		try {
+			final WebSocket self = this;
+			final OkHttpClient client = new OkHttpClient();
+			if (this.sslContext != null) {
+				SSLSocketFactory factory = sslContext.getSocketFactory();// (SSLSocketFactory) SSLSocketFactory.getDefault();
+				client.setSslSocketFactory(factory);
+			}
+			if (this.hostnameVerifier != null) {
+				client.setHostnameVerifier(this.hostnameVerifier);
+			}
+			Request.Builder builder = new Request.Builder().url(uri());
+			for (Map.Entry<String, String> entry : headers.entrySet()) {
+				builder.addHeader(entry.getKey(), entry.getValue());
+			}
+			final Request request = builder.build();
+			(wsCall = WebSocketCall.create(client, request)).enqueue(new WebSocketListener() {
+				@Override
+				public void onOpen(com.squareup.okhttp.ws.WebSocket webSocket, Response response) {
+					ws = webSocket;
+					final Map<String, String> headers = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+					Headers responseHeaders = response.headers();
+					for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+						headers.put(responseHeaders.name(i), responseHeaders.value(i));
+					}
+					EventThread.exec(new Runnable() {
+						@Override
+						public void run() {
+							self.emit(EVENT_RESPONSE_HEADERS, headers);
+							self.onOpen();
+						}
+					});
+				}
 
-        final WebSocket self = this;
-        final OkHttpClient client = new OkHttpClient();
-        if (this.sslContext != null) {
-            SSLSocketFactory factory = sslContext.getSocketFactory();// (SSLSocketFactory) SSLSocketFactory.getDefault();
-            client.setSslSocketFactory(factory);
-        }
-        if (this.hostnameVerifier != null) {
-            client.setHostnameVerifier(this.hostnameVerifier);
-        }
-        Request.Builder builder = new Request.Builder().url(uri());
-        for (Map.Entry<String, String> entry : headers.entrySet()) {
-            builder.addHeader(entry.getKey(), entry.getValue());
-        }
-        final Request request = builder.build();
-        (wsCall = WebSocketCall.create(client, request)).enqueue(new WebSocketListener() {
-            @Override
-            public void onOpen(com.squareup.okhttp.ws.WebSocket webSocket, Response response) {
-                ws = webSocket;
-                final Map<String, String> headers = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
-                Headers responseHeaders = response.headers();
-                for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-                    headers.put(responseHeaders.name(i), responseHeaders.value(i));
-                }
-                EventThread.exec(new Runnable() {
-                    @Override
-                    public void run() {
-                        self.emit(EVENT_RESPONSE_HEADERS, headers);
-                        self.onOpen();
-                    }
-                });
-            }
+				@Override
+				public void onMessage(BufferedSource payload, final PayloadType type) throws IOException {
+					Object data = null;
+					switch (type) {
+					case TEXT:
+						data = payload.readUtf8();
+						break;
+					case BINARY:
+						data = payload.readByteArray();
+						break;
+					default:
+						EventThread.exec(new Runnable() {
+							@Override
+							public void run() {
+								self.onError("Unknown payload type: " + type, new IllegalStateException());
+							}
+						});
+					}
+					payload.close();
+					final Object finalData = data;
+					EventThread.exec(new Runnable() {
+						@Override
+						public void run() {
+							if (finalData == null) {
+								return;
+							}
+							if (finalData instanceof String) {
+								self.onData((String) finalData);
+							} else {
+								self.onData((byte[]) finalData);
+							}
+						}
+					});
 
-            @Override
-            public void onMessage(BufferedSource payload, final PayloadType type) throws IOException {
-                Object data = null;
-                switch (type) {
-                    case TEXT:
-                        data = payload.readUtf8();
-                        break;
-                    case BINARY:
-                        data = payload.readByteArray();
-                        break;
-                    default:
-                        EventThread.exec(new Runnable() {
-                            @Override
-                            public void run() {
-                                self.onError("Unknown payload type: " + type, new IllegalStateException());
-                            }
-                        });
-                }
-                payload.close();
-                final Object finalData = data;
-                EventThread.exec(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (finalData == null) {
-                            return;
-                        }
-                        if (finalData instanceof String) {
-                            self.onData((String) finalData);
-                        } else {
-                            self.onData((byte[]) finalData);
-                        }
-                    }
-                });
+				}
 
-            }
+				@Override
+				public void onPong(Buffer payload) {
+				}
 
-            @Override
-            public void onPong(Buffer payload) {
-            }
+				@Override
+				public void onClose(int code, String reason) {
+					EventThread.exec(new Runnable() {
+						@Override
+						public void run() {
+							self.onClose();
+						}
+					});
+				}
 
-            @Override
-            public void onClose(int code, String reason) {
-                EventThread.exec(new Runnable() {
-                    @Override
-                    public void run() {
-                        self.onClose();
-                    }
-                });
-            }
+				@Override
+				public void onFailure(final IOException e, final Response response) {
+					EventThread.exec(new Runnable() {
+						@Override
+						public void run() {
+							self.onError("websocket error", e);
+						}
+					});
+				}
+			});
+			client.getDispatcher().getExecutorService().shutdown();
 
-            @Override
-            public void onFailure(final IOException e, final Response response) {
-                EventThread.exec(new Runnable() {
-                    @Override
-                    public void run() {
-                        self.onError("websocket error", e);
-                    }
-                });
-            }
-        });
-        client.getDispatcher().getExecutorService().shutdown();
-    }
+		}catch (Exception e) {
+		}
+	}
 
-    protected void write(Packet[] packets) {
-        final WebSocket self = this;
-        this.writable = false;
-        for (Packet packet : packets) {
-            Parser.encodePacket(packet, new Parser.EncodeCallback() {
-                @Override
-                public void call(Object packet) {
-                    try {
-                        if (packet instanceof String) {
-                            self.ws.sendMessage(TEXT, new Buffer().writeUtf8((String) packet));
-                        } else if (packet instanceof byte[]) {
-                            self.ws.sendMessage(BINARY, new Buffer().write((byte[]) packet));
-                        }
-                    } catch (Exception e) {
-                        self.onError("websocket error", e);
-                    }
-                }
-            });
-        }
+	protected void write(Packet[] packets) {
+		final WebSocket self = this;
+		this.writable = false;
+		for (Packet packet : packets) {
+			Parser.encodePacket(packet, new Parser.EncodeCallback() {
+				@Override
+				public void call(Object packet) {
+					try {
+						if (packet instanceof String) {
+							self.ws.sendMessage(TEXT, new Buffer().writeUtf8((String) packet));
+						} else if (packet instanceof byte[]) {
+							self.ws.sendMessage(BINARY, new Buffer().write((byte[]) packet));
+						}
+					} catch (Exception e) {
+						self.onError("websocket error", e);
+					}
+				}
+			});
+		}
 
-        final Runnable ondrain = new Runnable() {
-            @Override
-            public void run() {
-                self.writable = true;
-                self.emit(EVENT_DRAIN);
-            }
-        };
+		final Runnable ondrain = new Runnable() {
+			@Override
+			public void run() {
+				self.writable = true;
+				self.emit(EVENT_DRAIN);
+			}
+		};
 
-        // fake drain
-        // defer to next tick to allow Socket to clear writeBuffer
-        EventThread.nextTick(ondrain);
-    }
+		// fake drain
+		// defer to next tick to allow Socket to clear writeBuffer
+		EventThread.nextTick(ondrain);
+	}
 
-    @Override
-    protected void onClose() {
-        super.onClose();
-    }
+	@Override
+	protected void onClose() {
+		super.onClose();
+	}
 
-    protected void doClose() {
-        if (wsCall != null) {
-            wsCall.cancel();
-            wsCall = null;
-        }
-        if (ws != null) {
-            try {
-                ws.close(1000, "");
-            } catch (IllegalStateException e) {
-                // do nothing
-            } catch (Exception e) {
-                onError("websocket error", e);
-            }
-            ws = null;
-        }
-    }
+	protected void doClose() {
+		if (wsCall != null) {
+			wsCall.cancel();
+			wsCall = null;
+		}
+		if (ws != null) {
+			try {
+				ws.close(1000, "");
+			} catch (IllegalStateException e) {
+				// do nothing
+			} catch (Exception e) {
+				onError("websocket error", e);
+			}
+			ws = null;
+		}
+	}
 
-    protected String uri() {
-        Map<String, String> query = this.query;
-        if (query == null) {
-            query = new HashMap<String, String>();
-        }
-        String schema = this.secure ? "wss" : "ws";
-        String port = "";
+	protected String uri() {
+		Map<String, String> query = this.query;
+		if (query == null) {
+			query = new HashMap<String, String>();
+		}
+		String schema = this.secure ? "wss" : "ws";
+		String port = "";
 
-        if (this.port > 0 && (("wss".equals(schema) && this.port != 443)
-                || ("ws".equals(schema) && this.port != 80))) {
-            port = ":" + this.port;
-        }
+		if (this.port > 0 && (("wss".equals(schema) && this.port != 443)
+				|| ("ws".equals(schema) && this.port != 80))) {
+			port = ":" + this.port;
+		}
 
-        if (this.timestampRequests) {
-            query.put(this.timestampParam, String.valueOf(new Date().getTime()));
-        }
+		if (this.timestampRequests) {
+			query.put(this.timestampParam, String.valueOf(new Date().getTime()));
+		}
 
-        String _query = ParseQS.encode(query);
-        if (_query.length() > 0) {
-            _query = "?" + _query;
-        }
+		String _query = ParseQS.encode(query);
+		if (_query.length() > 0) {
+			_query = "?" + _query;
+		}
 
-        return schema + "://" + this.hostname + port + this.path + _query;
-    }
+		return schema + "://" + this.hostname + port + this.path + _query;
+	}
 }
